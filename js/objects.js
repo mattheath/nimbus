483a3d27ad8a2a8c2ce3645dd18e64523b25871d
Matt Heath <matt@mattheath.com>
2013-04-21 20:09:13 +0100

    Add first version of ELB octagonal prism

--- js/objects.js
+++ js/objects.js
@@ -95,3 +95,111 @@ function createCube(width, height, x, y, z, opacity) {
      return cube;
 
  }
+
+
+function createOctagon(scene, width, height, x, y, z, opacity) {
+
+     // Container object
+     var octagon = new THREE.Object3D(); //create an empty container
+
+     // Adjust registration point to bottom of object
+     y = y + height / 2;
+
+     var cornerRadius = (width - (width / (1 + Math.sqrt(2)))) / 2;
+     console.log(cornerRadius);
+
+     // Boundaries
+     var xMin = x - width / 2;
+     var xMax = x + width / 2;
+     var zMin = z - width / 2;
+     var zMax = z + width / 2;
+     var yMin = y;
+     var yMax = y + height;
+
+     // Calculate vertices
+     var v0 = new THREE.Vector3(xMax - cornerRadius, yMin, zMin);
+     var v1 = new THREE.Vector3(xMin + cornerRadius, yMin, zMin);
+     var v2 = new THREE.Vector3(xMin, yMin, zMin + cornerRadius);
+     var v3 = new THREE.Vector3(xMin, yMin, zMax - cornerRadius);
+     var v4 = new THREE.Vector3(xMin + cornerRadius, yMin, zMax);
+     var v5 = new THREE.Vector3(xMax - cornerRadius, yMin, zMax);
+     var v6 = new THREE.Vector3(xMax, yMin, zMax - cornerRadius);
+     var v7 = new THREE.Vector3(xMax, yMin, zMin + cornerRadius);
+
+     var v8  = new THREE.Vector3(xMax - cornerRadius, yMax, zMin);
+     var v9  = new THREE.Vector3(xMin + cornerRadius, yMax, zMin);
+     var v10 = new THREE.Vector3(xMin, yMax, zMin + cornerRadius);
+     var v11 = new THREE.Vector3(xMin, yMax, zMax - cornerRadius);
+     var v12 = new THREE.Vector3(xMin + cornerRadius, yMax, zMax);
+     var v13 = new THREE.Vector3(xMax - cornerRadius, yMax, zMax);
+     var v14 = new THREE.Vector3(xMax, yMax, zMax - cornerRadius);
+     var v15 = new THREE.Vector3(xMax, yMax, zMin + cornerRadius);
+
+     // Start building our Geometry
+     var geometry = new THREE.Geometry();
+
+     // Push in all the vertices
+     geometry.vertices.push(v0);
+     geometry.vertices.push(v1);
+     geometry.vertices.push(v2);
+     geometry.vertices.push(v3);
+     geometry.vertices.push(v4);
+     geometry.vertices.push(v5);
+     geometry.vertices.push(v6);
+     geometry.vertices.push(v7);
+
+     geometry.vertices.push(v8);
+     geometry.vertices.push(v9);
+     geometry.vertices.push(v10);
+     geometry.vertices.push(v11);
+     geometry.vertices.push(v12);
+     geometry.vertices.push(v13);
+     geometry.vertices.push(v14);
+     geometry.vertices.push(v15);
+
+     // Add faces, top and bottom need 3 polygons as we can only create faces
+     // with 4 vertices
+
+     // Bottom face
+     geometry.faces.push(new THREE.Face4(0, 1, 2, 3));
+     geometry.faces.push(new THREE.Face4(0, 3, 4, 7));
+     geometry.faces.push(new THREE.Face4(4, 5, 6, 7));
+
+     // Top face
+     geometry.faces.push(new THREE.Face4(8, 9, 10, 11));
+     geometry.faces.push(new THREE.Face4(8, 11, 12, 15));
+     geometry.faces.push(new THREE.Face4(12, 13, 14, 15));
+
+     // And each side
+
+     geometry.faces.push(new THREE.Face4(0, 1, 9, 8));
+     geometry.faces.push(new THREE.Face4(1, 2, 10, 9));
+     geometry.faces.push(new THREE.Face4(2, 3, 11, 10));
+     geometry.faces.push(new THREE.Face4(3, 4, 12, 11));
+     geometry.faces.push(new THREE.Face4(4, 5, 13, 12));
+     geometry.faces.push(new THREE.Face4(5, 6, 14, 13));
+     geometry.faces.push(new THREE.Face4(6, 7, 15, 14));
+     geometry.faces.push(new THREE.Face4(7, 0, 8, 15));
+
+
+     console.log(geometry);
+
+
+     var posX = x + width / 2;
+     var posY = y + 5;
+     var posZ = z + width / 2;
+
+     var material = new THREE.MeshBasicMaterial( { color: 0xE6E6E6, side: THREE.DoubleSide, opacity: opacity, transparent: true } );
+     var mesh = new THREE.Mesh(geometry, material);
+     mesh.position.set(posX, posY, posZ);
+     octagon.add( mesh );
+
+
+     console.log(octagon);
+
+     return octagon;
+
+}
+
+
+
