efde128a16d20b23b649073537815918cfe83fce
Matt Heath <matt@mattheath.com>
2014-07-28 23:17:42 +0100

    Add very basic S3 shape

--- js/objects/ec2.js
+++ js/objects/ec2.js
@@ -1,7 +1,7 @@
 
 /**
-* Builds an EC2 Instance object
-*/
+ * Builds an EC2 Instance object
+ */
 EC2Instance = function (origin, size) {
 
   // Set height based on instance size
@@ -34,4 +34,35 @@ EC2Instance = function (origin, size) {
   i.push(l)
 
   return i
+}
+
+/**
+ * Builds a rudimetary S3 object
+ */
+S3 = function (origin) {
+
+  // Make a collection object to hold everything
+  var s = new Isomer.Object3D()
+
+  // Sizing
+  var w = 1.5;
+  var h = w;
+  var l = w * 1.5;
+
+  // Make our base shape
+  var base = Isomer.Shape.Prism(origin, l, w, h)
+  base.setColor(new Isomer.Color(160, 160, 160))
+  s.push(base);
+
+  // Add a logo on top - currenly a square
+  var l = new Isomer.Path([
+    origin.translate(l/2 - 0.2, w/2 - 0.2, h),
+    origin.translate(l/2 + 0.2, w/2 - 0.2, h),
+    origin.translate(l/2 + 0.2, w/2 + 0.2, h),
+    origin.translate(l/2 - 0.2, w/2 + 0.2, h)
+  ])
+  l.setColor(new Isomer.Color(100, 100, 100))
+  s.push(l)
+
+  return s
 }
