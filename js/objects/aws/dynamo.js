88ce787172c88244f1470e6b072e9803b0a010d7
Matt Heath <matt@mattheath.com>
2014-08-03 00:24:15 +0100

    Specify sizings for dynamo icon using cross primative

--- js/objects/aws/dynamo.js
+++ js/objects/aws/dynamo.js
@@ -8,5 +8,23 @@
  * DynamoDB returns an icon representing a DynamoDB instance
  */
 DynamoDB = function (origin) {
-	return new OutlinedCross(origin)
+
+	// Sizing settings for the DynamoDB icon
+	var w = 2;         // width
+	var h = w*0.75;    // overall height
+	var c = 0.5;       // corner radius
+	var t = h - c*0.5; // height of top (centre)
+
+	// Get base shape
+	var ddb = new OutlinedCross(origin, w, h, c, t)
+
+	// Add a logo on top
+	ddb.push(new Isomer.Path([
+		origin.translate(-0.3, 0.3, t),
+		origin.translate(0.3, 0.3, t),
+		origin.translate(0.3, -0.3, t),
+		origin.translate(-0.3, -0.3, t),
+	]).reverse(), new Isomer.Color(72, 72, 72));
+
+	return ddb
 }
